a0c11bf37a43d35bb9b0717758c13dadb9bf68d8
SupKittyMeow <supkittymeow@gmail.com>
2025-11-02 20:29:21 -0800

    switch from supabase to upstash-redis because supabase kept pausing my project and im too lazy to take 1 second a month to unpause it

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-#TODO: show which character was used
-#TODO: show leaderboard based on argument1 which is starting point
+# TODO: show which character was used
+# TODO: show leaderboard based on argument1 which is starting point
 
-from supabase import create_client, Client
 from warnings import filterwarnings
+from upstash_redis import Redis
 import scratchattach as sa
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+redis = Redis.from_env()
+
 filterwarnings('ignore', category=sa.LoginDataWarning)
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_SERVICE_ROLE")
-session_id = os.getenv("SCRATCH_SESSION_ID")
-
-if url is None or key is None:
-    raise ValueError("Supabase URL and KEY must be set in environment variables.")
-if session_id is None:
-    raise ValueError("SESSION_ID must be set in environment variables.")
-
-session = sa.login_by_id(session_id, username="SupKittyMeow")
+session = sa.login_by_id(str(os.getenv("SCRATCH_SESSION_ID")), username="SupKittyMeow")
 cloud = session.connect_cloud("1175964459")
 client = cloud.requests()
-
-supabase: Client = create_client(url, key)
 
 @client.event
 def on_ready(): # just to make sure everything is working
@@ -36,48 +27,40 @@ def ping(): # sends back 'pong' to the Scratch project
 
 @client.request
 def add_score(argument1, argument2): # sets the score of the user to the second argument, saved to a database
-    data = {"Username": argument1, "Max kills": argument2}
-    supabase.table("leaderboard").upsert(data).execute()
+    try:
+        score = int(argument2)
+    except ValueError:
+        return "Error: Score must be a number!"
+    
+    redis.zadd('leaderboard', {argument1: score} )
     return "score set"
 
 @client.request
 def get_score(argument1): # retrieve a user's score
-    response = supabase.table("leaderboard").select("*").eq("Username", argument1).execute() 
+    response = redis.zscore('leaderboard', argument1)
+
     # Check if the player exists in the leaderboard
-    if response.data:
+    if response is not None:
         # If player exists, return the data
-        return int(response.data[0].get("Max kills", 0))
+        return int(response)
     else:
         # If player doesn't exist, return 0
         return 0
     
 @client.request
 def reset_score(argument1): # deletes the user's score from the database
-    supabase.table("leaderboard").delete().eq("Username", argument1).execute()
+    redis.delete(argument1)
     return "RESET"
 
 @client.request
 def get_leaderboard(leaderboardStart): # returns a list of the top 10 scores
     leaderboardStart = int(leaderboardStart)
-    response = (
-        supabase
-        .table("leaderboard")
-        .select('"Username","Max kills"')
-        .order('"Max kills"', desc=True)
-        .order("Username", desc=False)
-        .range(leaderboardStart, leaderboardStart + 4)
-        .execute()
-    )
 
-    # Get total count of rows (excluding header, if you have one as a row)
-    count_response = supabase.table("leaderboard").select("Username", count="exact").execute() #type: ignore
-    total_players = count_response.count or 0
+    descending_users = redis.zrange('leaderboard', leaderboardStart, leaderboardStart + 4, withscores=True)
 
-    if response.data:
-        leaderboard_list = [
-            f"{item['Username']}: {item['Max kills']}" for item in response.data
-        ]
-        leaderboard_list.append(str(total_players))
+    if descending_users is not None:
+        leaderboard_list = [ f"{user[0]}: {int(user[1])}" for user in descending_users ]
+        leaderboard_list.append(str(len(descending_users)))
         return leaderboard_list
     else:
         return []
